76a129e306cb98359d814cddfdf0e2d66ac0019b
User <User@LAB3-3>
2024-01-24 09:35:12 +0200

    changes

--- Binding/MainPage.xaml.cs
+++ Binding/MainPage.xaml.cs
@@ -1,14 +1,21 @@
-﻿namespace Binding
+﻿using Binding.Models;
+
+namespace Binding
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
+        public Student s { get; set; }
         public MainPage()
         {
+            s = new Student() { Name = "Itamar", Age = 16 };
+            this.BindingContext = s;
             InitializeComponent();
         }
 
-      
+        private void ChangeName(object sender, EventArgs e)
+        {
+            s.Name = "Ben";
+        }
     }
 }
